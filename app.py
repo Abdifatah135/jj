07754883b6f5dd08520037dcee5234ece13c1365
Abdi <oxabdi75@gmail.com>
2020-01-07 16:52:19 +0300

    added article

--- app.py
+++ app.py
@@ -10,3 +10,16 @@ def Index():
     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
     
     articles = topheadlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+ 
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
